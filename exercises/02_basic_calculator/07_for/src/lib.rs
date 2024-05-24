506b5a5531a6823f0496c13a7321722c256d9808
Ante Perisic <perisic.ante@gmail.com>
2024-05-24 14:03:34 +0200

    ex02_7

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+       return 1;
+   }
+   let mut res = 1;
+   for i in 1..=n {
+       res *= i;
+   }
+   res
 }
 
 #[cfg(test)]
