d65e2733c00217607f382f6049d009037ec59735
Ante Perisic <perisic.ante@google.com>
2024-09-09 14:52:43 +0200

    impl tryfrom with thisseror

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,11 +1,39 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
 //  The parsing should be case-insensitive.
 
+
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("{bad_status} is not valid")]
+struct StatusError {
+    bad_status: String,
+}
+
+impl TryFrom<String> for Status {
+    type Error = StatusError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = StatusError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        
+        match value.to_lowercase().as_str() {
+            "todo" => {Ok(Self::ToDo)},
+            "inprogress" => {Ok(Status::InProgress)},
+            "done" => {Ok(Status::Done)},
+            _ => Err(StatusError{bad_status: value.to_string()}),
+            
+        }
+    }
 }
 
 #[cfg(test)]
