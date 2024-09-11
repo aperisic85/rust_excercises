6f9e2897bd246e11911daade993d0bf1a1c13c59
Ante Perisic <perisic.ante@google.com>
2024-09-11 09:10:30 +0200

    15 outro

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -23,6 +23,11 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("{p}")]
+    BadStatus{
+        #[from]
+        p : status::ParseStatusError
+    },
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -48,6 +53,8 @@ impl Ticket {
         }
 
         // TODO: Parse the status string into a `Status` enum.
+
+        let status = status::Status::try_from(status)?;
 
         Ok(Ticket {
             title,
