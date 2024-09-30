aaf551f39e0317d0c9bac5dab2353516b676ec9e
Ante Perisic <perisic.ante@google.com>
2024-09-30 10:46:33 +0200

    impl into_iter()

--- exercises/06_ticket_management/04_iterators/src/lib.rs
+++ exercises/06_ticket_management/04_iterators/src/lib.rs
@@ -1,3 +1,4 @@
+
 use ticket_fields::{TicketDescription, TicketTitle};
 
 // TODO: Let's start sketching our ticket store!
@@ -8,6 +9,15 @@ use ticket_fields::{TicketDescription, TicketTitle};
 #[derive(Clone)]
 pub struct TicketStore {
     tickets: Vec<Ticket>,
+}
+
+impl IntoIterator for TicketStore {
+    type Item = Ticket;
+    type IntoIter = std::vec::IntoIter<Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.into_iter()
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
